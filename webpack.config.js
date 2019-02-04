4a6b32dba02a033c922eaaa4e391d68a216ecbea
Akinjide Bankole <r@akinjide.me>
2019-02-04 12:19:35 +0100

    refactor: change extension run at and add browser polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,9 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: './extension/main.js',
-    content: './extension/content.js'
+    main: './extension/main',
+    content: './extension/content',
+    options: './extension/options'
   },
   output: {
     path: path.join(__dirname, 'extension/build'),
@@ -33,6 +34,10 @@ module.exports = {
       {
         from: 'images/*',
         context: 'extension'
+      },
+      {
+        from: 'webextension-polyfill/dist/browser-polyfill.min.js',
+        context: 'node_modules'
       },
       {
         from: 'jquery/dist/jquery.slim.min.js',
