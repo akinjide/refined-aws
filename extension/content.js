2097b8b55cad505c021905516788fd35c7ca9f66
Akinjide Bankole <r@akinjide.me>
2019-02-04 12:18:08 +0100

    refactor: add observer for route change and extension logging

--- extension/content.js
+++ extension/content.js
@@ -1,19 +1,66 @@
-import {runOnInitFeatures} from './features';
+import {runOnInitFeatures, features} from './features';
+import {StoreSync} from './library/store-sync';
+import {ready, observeElement, safeElementReady} from './library/dom';
 
-async function init() {
-  for (const feature of runOnInitFeatures) {
-    enableFeature(feature);
+const storeSync = new StoreSync(window, 'refined:aws');
+let options;
+
+async function enableFeature({fn = (() => {}), id}) {
+  let log = () => {};
+
+  try {
+    if (!options) {
+      options = await storeSync.get(null);
+    }
+
+    const {logging = false} = options;
+
+    if (logging) {
+      log = console.log;
+    }
+
+    if (options[id] === false) {
+      return log('⚠️', 'Skipping', id);
+    }
+
+    await fn(log);
+    log('✅', id);
+  } catch (error) {
+    log('❓', id);
+    log(error);
   }
 }
 
-async function enableFeature({fn, id}) {
-  try {
-    await fn();
-    console.log('✅', id);
-  } catch (error) {
-    console.log('❌', id);
-    console.error(error);
+function onRouteChange(callback) {
+  observeElement('#b', callback, {attributes: true, childList: true, characterData: true});
+}
+
+function observeAndEnableFeatures() {
+  enableFeature(features.keyboardShortcuts);
+
+  onRouteChange(mutations => {
+    console.log(mutations);
+    enableFeature(features.hideAmazonConsoleLogo);
+    enableFeature(features.hideSupport);
+    enableFeature(features.hideRegion);
+    enableFeature(features.hideFooter);
+    enableFeature(features.hideResourceGroups);
+    enableFeature(features.showSignOutButton);
+    enableFeature(features.showDocumentationButton);
+    enableFeature(features.duplicateBreadCrumbNavigation);
+  });
+}
+
+async function init() {
+  await safeElementReady('body');
+
+  for (const feature of runOnInitFeatures) {
+    enableFeature(feature);
   }
+
+  await ready();
+
+  observeAndEnableFeatures();
 }
 
 init();
